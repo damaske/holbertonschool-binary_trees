6d0fcf93e9367b9b465228110c56fa2b32f23576
Ahmed <edtaghiyev@gmail.com>
2024-12-12 20:05:12 +0400

    Fix

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    int *head;
+    binary_tree_t *newNode;
+    newNode = (binary_tree_t*)malloc(sizeof(binary_tree_t));
+
+    if (newNode == NULL)
+    {
+        printf("Memory error.\n");
+        return NULL;
+    }
+
+    newNode->n = value;
+    newNode->parent = parent;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    if (newNode->parent == NULL)
+    {
+        return NULL;
+    }
+
+    return newNode;
+}
