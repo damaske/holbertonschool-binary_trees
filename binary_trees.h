a28be5d8abc109bc6f81c9e7c863fec786f94e86
Ahmed <edtaghiyev@gmail.com>
2024-12-12 20:30:55 +0400

    hi

--- binary_trees.h
+++ binary_trees.h
@@ -3,6 +3,7 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <stddef.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -41,10 +42,5 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
-
-typedef struct binary_tree_s binary_tree_t;
-typedef struct binary_tree_s avl_t;
-typedef struct binary_tree_s bst_t;
-typedef struct binary_tree_s heap_t;
 
 #endif
