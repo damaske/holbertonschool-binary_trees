74cd64439b4417351504a45f71d3c330458fffde
damaske <damaske.vzk@gmail..com>
2024-12-12 18:30:18 +0000

    1. Insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -3,7 +3,6 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_left;
-	int temp;
 
 	new_left = malloc(sizeof(binary_tree_t));
 	if (new_left == NULL)
