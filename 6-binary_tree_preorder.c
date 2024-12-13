5aa70cf6e085e472489a5a2da53ff07c6ef51582
Ahmed <edtaghiyev@gmail.com>
2024-12-13 19:56:48 +0400

    hi

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - goes through a binary
+ * @tree: pointer to root
+ * Return: Nothing
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+    if (tree || func == NULL)
+    {
+        return;
+    }
+
+    binary_tree_preorder(tree->left);
+    binary_tree_preorder(tree->right);
+    
+}
