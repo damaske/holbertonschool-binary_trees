e441dc2522a3290dd7473bff4d64883a1be51ebc
damaske <damaske.vzk@gmail.com>
2024-12-13 15:47:10 +0000

    5. Is root

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_root - checkes if a given node is root
+ * @node: pointer to node
+ * Return: if NULL 1 otherwise 0
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->parrent == NULL)
+		return (1);
+
+	return (0);
+}
