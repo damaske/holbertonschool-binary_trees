72a874a28bd460069d10368de427cfe32397d665
Ahmed <edtaghiyev@gmail.com>
2024-12-14 22:04:33 +0400

    help

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -11,6 +11,9 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
+	if (tree->left == NULL || tree->right == NULL)
+		return (1);
+
 	left = binary_tree_size(tree->left);
 	right = binary_tree_size(tree->right);
 
